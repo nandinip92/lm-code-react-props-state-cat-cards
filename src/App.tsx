21ffce36c6df463889551a62d2bb10158f2b34e0
nandinip92 <nandini.p8892@gmail.com>
2023-08-31 15:42:00 +0100

    feat: Looping through cats to render <CatCard/>

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,9 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          <CatCard />
+          {cats.map((cat) => (
+            <CatCard />
+          ))}
         </div>
       </main>
 
