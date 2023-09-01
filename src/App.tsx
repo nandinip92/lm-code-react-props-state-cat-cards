f8fc8b098c21315596b2fada31352696fddf2355
nandinip92 <nandini.p8892@gmail.com>
2023-09-01 19:26:12 +0100

    feat: Adding licence attributes to <CatImage>

--- src/App.tsx
+++ src/App.tsx
@@ -93,8 +93,8 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          {cats.map((cat) => (
-            <CatCard catObject={cat} />
+          {cats.map((cat, index) => (
+            <CatCard catObject={cat} catIndex={index} />
           ))}
           {/* {cats.map((cat) => (
             <CatCard
